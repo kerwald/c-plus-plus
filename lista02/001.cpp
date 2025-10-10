e70aaf5fa151199727d43b5dbfefe48df26bafa4
Wald <kerwald.s@gmail.com>
2025-10-10 00:49:08 -0300

    Add C++ exercises demonstrating class usage, encapsulation, and method implementation

---
+++ lista02/001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Pessoa {
+
+    public:
+        std::string nome;
+        int idade;
+
+};
+
+int main(){
+
+}
---
+++ lista02/005.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Pessoa {
+
+    private:
+        std::string nome;
+        int idade;
+
+    public:
+
+        void apresentar(){
+            std::cout << "Ola, meu nome e " << nome << " e eu tenho " << idade << " anos" << std::endl;
+        }
+
+};
+
+int main(){
+
+    Pessoa Joao /*{ "Felipe", 22 }*/;
+
+    std::cout << /* Joao.nome */ "Felipe" << " " << /*Joao.idade*/ "22" << std::endl; // Erro de compilacao, os atributos sao privates, so podem ser modificados no escopo da classe
+    Joao.apresentar();
+
+}
