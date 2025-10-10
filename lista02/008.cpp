54557df734c58cc947d686286db9c145a66f5acd
Wald <kerwald.s@gmail.com>
2025-10-10 13:32:13 -0300

    Add C++ exercises demonstrating class implementation, encapsulation, and method usage

---
+++ lista02/008.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Pessoa {
+
+    private:
+        std::string nome;
+        int idade;
+
+    public:
+
+        void setNome( std::string nome ){
+            this->nome = nome;
+        }
+
+        void setIdade( int idade ){
+            this->idade = idade;
+        }
+
+        std::string getNome(){
+            return nome;
+        }
+
+        int getIdade(){
+            return idade;
+        }
+        
+        void apresentar(){
+            std::cout << "Ola, meu nome e " << nome << " e eu tenho " << idade << " anos" << std::endl;
+        }
+
+};
+
+int main(){
+
+    Pessoa Joao;
+    Joao.setIdade( 22 );
+    Joao.setNome( "Joao Kerwald" );
+
+    std::cout << "nome: " << Joao.getNome() << " idade: " << Joao.getIdade();
+
+}
